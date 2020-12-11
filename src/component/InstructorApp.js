9bbec867ede77e99afd70779ff5523c8a649f9e7
tlanders <768713+tlanders@users.noreply.github.com>
2020-12-11 17:18:57 -0600

    added axios and others added bootstrap to html started two components

---
+++ src/component/InstructorApp.js
@@ -0,0 +1,17 @@
+import React,
+    {Component}
+    from 'react';
+import ListCoursesComponent from "./ListCoursesComponent";
+
+class InstructorApp extends Component {
+    render() {
+        return (
+            <>
+                <h1>Instructor Application</h1>
+                <ListCoursesComponent/>
+            </>
+        );
+    }
+}
+
+export default InstructorApp;
