678b09960aa68625054400152c075ee832474e7d
tlanders <768713+tlanders@users.noreply.github.com>
2020-12-12 15:34:31 -0600

    added axios service to call sameplepai

--- src/component/ListCoursesComponent.js
+++ src/component/ListCoursesComponent.js
@@ -1,6 +1,25 @@
 import React, {Component} from 'react';
+import CourseDataService  from "../service/CourseDataService";
+
+const INSTRUCTOR = 'in28minutes';
 
 class ListCoursesComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.refreshCourses = this.refreshCourses.bind(this);
+    }
+
+    componentDidMount() {
+        this.refreshCourses();
+    }
+
+    refreshCourses() {
+        CourseDataService.retrieveAllCourses(INSTRUCTOR)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
     render() {
         return (
             <div className={'container'}>
